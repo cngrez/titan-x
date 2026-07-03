aecb1be84944ac0aa44d430b7f9bb6c11ad7bae6
chuacn1 <chuacn1@student.op.ac.nz>
2026-07-03 12:02:37 +1200

    feat(jwt): add role at the create JWT acces tooken function

--- backend/src/utils/jwt.py
+++ backend/src/utils/jwt.py
@@ -9,10 +9,11 @@ SECRET_KEY = os.getenv("JWT_SECRET", "super-secret-titan-x-hihi")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 2  # 2 hours
 
-def create_access_token(user_id: int) -> str:
+def create_access_token(user_id: int, role: str) -> str:
     """Create a JWT access token."""
     payload = {
         "sub": str(user_id),
+        "role": role,
         "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
