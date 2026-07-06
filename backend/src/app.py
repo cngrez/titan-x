f451ea9c070beae67c338460b8d8b9e86434bb11
chuacn1 <chuacn1@student.op.ac.nz>
2026-07-06 14:21:01 +1200

    feat(app.py): add body metric into app.py) git push

--- backend/src/app.py
+++ backend/src/app.py
@@ -6,6 +6,7 @@ from routes.auth import router as auth_router
 from routes.user import router as user_router
 from routes.exercise import router as exercise_router
 from routes.routine import router as routine_router
+from routes.body_metrics import router as body_metrics_router
 
 def create_app():
     app = FastAPI(
@@ -32,6 +33,8 @@ def create_app():
     app.include_router(user_router, prefix="/api")
     app.include_router(exercise_router, prefix="/api")
     app.include_router(routine_router, prefix="/api")
+    app.include_router(body_metrics_router, prefix="/api")
+    
     return app
 
 app = create_app()
